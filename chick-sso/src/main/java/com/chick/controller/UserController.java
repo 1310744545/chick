7ee075ba0aebc05af5b6409c4a56a72835936cc6
xkx <1310744545@qq.com>
2023-02-28 17:17:29 +0800

    update

--- chick-sso/src/main/java/com/chick/controller/UserController.java
+++ chick-sso/src/main/java/com/chick/controller/UserController.java
@@ -52,13 +52,14 @@ public class UserController extends BaseController {
 
     /**
      * 登录
+     *
      * @param request 请求头
      * @return
      */
-    @ApiOperation(value = "登录", httpMethod = "POST")
+    @ApiOperation(value = "账号密码登录", httpMethod = "POST")
     @PostMapping("/login")
     public R login(@RequestBody LoginUserVO loginUserVO, HttpServletRequest request) {
-        if (ObjectUtils.isEmpty(loginUserVO)){
+        if (ObjectUtils.isEmpty(loginUserVO)) {
             return R.failed("系统错误");
         }
         if (StringUtils.isAnyBlank(loginUserVO.getUsername(), loginUserVO.getPassword())) {
@@ -72,13 +73,14 @@ public class UserController extends BaseController {
 
     /**
      * 微信登录
+     *
      * @param request 请求头
      * @return
      */
-    @ApiOperation(value = "微信登录", httpMethod = "POST")
+    @ApiOperation(value = "微信登录（只需账号）", httpMethod = "POST")
     @PostMapping("/loginWeChat")
     public R login(@RequestBody LoginWeChatUserVO loginWeChatUserVO, HttpServletRequest request) {
-        if (ObjectUtils.isEmpty(loginWeChatUserVO)){
+        if (ObjectUtils.isEmpty(loginWeChatUserVO)) {
             return R.failed("系统错误");
         }
         if (StringUtils.isAnyBlank(loginWeChatUserVO.getUsername())) {
@@ -88,20 +90,16 @@ public class UserController extends BaseController {
     }
 
     /**
-    * @Author xkx
-    * @Description 邮箱登录
-    * @Date 2023-02-12 21:34
-    * @Param [loginUserVO, request]
-    * @return com.chick.base.R
-    **/
-    @ApiOperation(value = "邮箱登录", httpMethod = "POST")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "email", value = "用户名", required = true, paramType = "query"),
-            @ApiImplicitParam(name = "code", value = "验证码", required = true, paramType = "query")
-    })
+     * @return com.chick.base.R
+     * @Author xkx
+     * @Description 邮箱登录
+     * @Date 2023-02-12 21:34
+     * @Param [loginUserVO, request]
+     **/
+    @ApiOperation(value = "邮箱登录（先发送邮箱登录验证码）", httpMethod = "POST")
     @PostMapping("/loginByEmail")
     public R loginByEmail(@RequestBody EmailUserVO emailUserVO, HttpServletRequest request) {
-        if (ObjectUtils.isEmpty(emailUserVO)){
+        if (ObjectUtils.isEmpty(emailUserVO)) {
             return R.failed("系统错误");
         }
         if (StringUtils.isAnyBlank(emailUserVO.getEmail())) {
@@ -115,17 +113,14 @@ public class UserController extends BaseController {
 
     /**
      * 用户名密码注册
+     *
      * @param registerUserVO 注册人信息
      * @return
      */
     @ApiOperation(value = "用户名密码注册", httpMethod = "POST")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "username", value = "用户名", required = true, paramType = "query"),
-            @ApiImplicitParam(name = "password", value = "密码", required = true, paramType = "query"),
-    })
     @PostMapping("/register")
     public R register(@RequestBody RegisterUserVO registerUserVO) {
-        if (ObjectUtils.isEmpty(registerUserVO)){
+        if (ObjectUtils.isEmpty(registerUserVO)) {
             return R.failed("系统错误");
         }
         if (StringUtils.isAnyBlank(registerUserVO.getUsername(), registerUserVO.getPassword())) {
@@ -139,13 +134,14 @@ public class UserController extends BaseController {
 
     /**
      * 邮箱注册
+     *
      * @param registerEmailUserVO 注册人信息
      * @return
      */
-    @ApiOperation(value = "邮箱注册", httpMethod = "POST")
+    @ApiOperation(value = "邮箱注册（先发送邮箱注册验证码）", httpMethod = "POST")
     @PostMapping("/registerEmail")
     public R registerEmail(@RequestBody RegisterEmailUserVO registerEmailUserVO) {
-        if (ObjectUtils.isEmpty(registerEmailUserVO)){
+        if (ObjectUtils.isEmpty(registerEmailUserVO)) {
             return R.failed("系统错误");
         }
         if (StringUtils.isAnyBlank(registerEmailUserVO.getEmail(), registerEmailUserVO.getCode())) {
@@ -156,6 +152,7 @@ public class UserController extends BaseController {
 
     /**
      * 退出登录,暂时没用,后期将token存到redis中时需要用这里先不用
+     *
      * @return
      */
     @ApiOperation(value = "退出登录", httpMethod = "GET")
@@ -166,8 +163,9 @@ public class UserController extends BaseController {
 
     /**
      * 获取验证码
-     * @param request 请求头
-     * @param response  响应头
+     *
+     * @param request  请求头
+     * @param response 响应头
      * @throws IOException
      */
     @ApiOperation(value = "验证码", httpMethod = "GET")
@@ -183,8 +181,8 @@ public class UserController extends BaseController {
             ImageIO.write(bi, "jpg", out);
             BASE64Encoder encoder = new BASE64Encoder();
             String encode = encoder.encode(out.toByteArray());
-            result.put("text",text);
-            result.put("captchaBase64",encode);
+            result.put("text", text);
+            result.put("captchaBase64", encode);
             return R.ok(result);
         } catch (Exception e) {
             response.sendError(HttpServletResponse.SC_NOT_FOUND);
@@ -194,12 +192,13 @@ public class UserController extends BaseController {
 
     /**
      * 通过jwt获取用户信息
+     *
      * @return
      */
     @ApiOperation(value = "获取用户信息", httpMethod = "POST")
     @PostMapping("/getUserByJwt")
     public R getUserByJwt(HttpServletRequest request) {
-        if (StringUtils.isBlank(request.getHeader(jwtUtils.getHeader()))){
+        if (StringUtils.isBlank(request.getHeader(jwtUtils.getHeader()))) {
             return R.failed("令牌不能为空");
         }
         return R.ok(userService.getUserByJwt(request.getHeader(jwtUtils.getHeader())));
@@ -211,42 +210,56 @@ public class UserController extends BaseController {
     })
     @PostMapping("/uploadHeadPortrait")
     @PreAuthorize(CommonConstants.HAS_ROLE_ADMIN)
-    public R uploadHeadPortrait(@RequestParam(name = "file") MultipartFile file){
-        if (ObjectUtils.isEmpty(file)){
+    public R uploadHeadPortrait(@RequestParam(name = "file") MultipartFile file) {
+        if (ObjectUtils.isEmpty(file)) {
             return R.failed("请上传文件");
         }
         return userService.uploadHeadPortrait(file, SecurityUtils.getUserId());
     }
 
-    @ApiOperation(value = "更换头像", position = 1, httpMethod = "POST")
+    @ApiOperation(value = "修改用户信息", position = 1, httpMethod = "POST")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "type", value = "文件类型id", paramType = "query"),
     })
     @PostMapping("/updateUser")
     @PreAuthorize(CommonConstants.HAS_ROLE_ADMIN)
-    public R updateUser(String userId, String sex, String phone, String name, String email, String birthday){
-        if (StringUtils.isBlank(userId)){
+    public R updateUser(String userId, String sex, String phone, String name, String email, String birthday) {
+        if (StringUtils.isBlank(userId)) {
             return R.failed("用户id为空");
         }
-        if (!getUserId().equals(userId)){
+        if (!getUserId().equals(userId)) {
             return R.failed("无权操作");
         }
         return userService.updateUser(userId, sex, phone, name, email, birthday);
     }
 
-
+    /**
+     * @return chick.sideline.common.base.R
+     * @Author xkx
+     * @Description 发送邮箱注册验证码
+     * @Date 2023-02-28 17:11
+     * @Param [email]
+     **/
+    @ApiOperation(value = "发送邮箱注册验证码", position = 1, httpMethod = "GET")
     @GetMapping("/sendEmailCodeRegister")
-    public R sendEmailCodeRegister(String email){
-        if (StringUtils.isBlank(email)){
+    public R sendEmailCodeRegister(String email) {
+        if (StringUtils.isBlank(email)) {
             return R.failed("邮件为空");
         }
         return userService.sendEmailCodeRegister(email);
     }
 
-    @ApiOperation("邮箱登录")
+    /**
+     * @Author xkx
+     * @Description 发送邮箱登录验证码
+     * @Date 2023-02-28 17:12
+     * @Param [email]
+     * @return chick.sideline.common.base.R
+     **/
+    @ApiOperation(value = "发送邮箱登录验证码", position = 1, httpMethod = "GET")
     @GetMapping("/sendEmailCodeLogin")
-    public R sendEmailCodeLogin(String email){
-        if (StringUtils.isBlank(email)){
+    public R sendEmailCodeLogin(String email) {
+        if (StringUtils.isBlank(email)) {
             return R.failed("邮件为空");
         }
         return userService.sendEmailCodeLogin(email);
