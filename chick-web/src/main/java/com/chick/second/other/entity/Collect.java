1747ba314c2b08a47a1ac673642330a1766775f3
xkx <1310744545@qq.com>
2023-03-07 14:19:52 +0800

    update

---
+++ chick-web/src/main/java/com/chick/second/other/entity/Collect.java
@@ -0,0 +1,67 @@
+package com.chick.second.other.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * <p>
+ * 收藏表
+ * </p>
+ *
+ * @author xiaokexin
+ * @since 2023-02-27
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class Collect implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * ID
+     */
+    @TableId(value = "id", type = IdType.ID_WORKER_STR)
+    private String id;
+
+    /**
+     * 用户id
+     */
+    private String userId;
+
+    /**
+     * 酒店id
+     */
+    private String hotelId;
+
+    /**
+     * 是否逻辑删除 1是0否
+     */
+    private String delFlag;
+
+    /**
+     * 创建者
+     */
+    private String createBy;
+
+    /**
+     * 创建时间
+     */
+    private Date createDate;
+
+    /**
+     * 更新者
+     */
+    private String updateBy;
+
+    /**
+     * 更新时间
+     */
+    private Date updateDate;
+
+
+}
