499c80082ee49d4fca7a4455dbe6c5d6f555117a
xkx <1310744545@qq.com>
2024-03-10 22:24:27 +0800

    基础框架修改，新增定时任务模块

--- chick-sso/src/main/java/com/chick/config/MailConfig.java
+++ chick-sso/src/main/java/com/chick/config/MailConfig.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
  * @ClassName MailConfig
  * @Author xiaokexin
  * @Date 2022-10-18 9:37
- * @Description 邮件发送配置类阿斯顿
+ * @Description 邮件发送配置类阿斯顿阿三大苏打现场
  * @Version 1.0
  */
 @Component
